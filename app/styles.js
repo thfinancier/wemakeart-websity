645f223d335e71490cdb233f13b80dc14f9f1279
The Financier <itisenward@gmail.com>
2024-07-14 21:45:29 +0200

    Initial setup

---
+++ app/styles.js
@@ -0,0 +1,7 @@
+const styles = {
+  boxWidth: 'xl:max-w-[1280px] w-full',
+  paddingX: 'sm:px-10 px-6',
+  paddingY: 'sm:py-10 py-8'
+}
+
+export default styles
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dimmed: 'hsl(0, 0%, 16%)',
+      },
+      fontFamily: {
+        syne: ['Syne', 'sans-serif'],
+      },
+      screens: {
+        xs: '480px',
+        ss: '620px',
+        sm: '768px',
+        md: '1060px',
+        lg: '1200px',
+        xl: '1700px',
       },
     },
   },
   plugins: [],
-};
+}
